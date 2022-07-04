71feb4edc033287bfc30236b28d6c7a9d4c1d39e
Ivan <ivan.spiridonov.01@inbox.ru>
2022-07-04 16:18:35 +0300

    delete index.php

---
+++ wp-content/plugins/tr-core/index.php
@@ -0,0 +1,13 @@
+<?php
+	
+/**
+ * Plugin Name: tr-core
+ * Description: Ядро темы themerage
+ *
+ *
+ * 
+ * License:     GPL2
+ * License URI: https://www.gnu.org/licenses/gpl-2.0.html
+ *
+ * Version:     1.0
+ */
